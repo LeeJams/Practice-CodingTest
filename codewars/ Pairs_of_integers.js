124efba662bd628ec35a0ef5f3c9eadf64dc348b
LeeJams <leejam208@gmail.com>
2021-09-15 17:55:21 +0900

    퇴근 전 짬나는 시간에 코테

---
+++ codewars/ Pairs_of_integers.js
@@ -0,0 +1,25 @@
+/* 
+https://www.codewars.com/kata/588e2a1ad1140d31cb00008c/train/javascript
+ Pairs_of_integers_from_m_to_n
+ 
+Implement a function that receives two integers m and n and 
+generates a sorted list of pairs (a, b) such that m <= a <= b <= n.
+
+Example:
+
+m = 2
+n = 4
+result = [ [ 2, 2 ], [ 2, 3 ], [ 2, 4 ], [ 3, 3 ], [ 3, 4 ], [ 4, 4 ] ]
+*/
+
+function generatePairs(m, n) {
+  const result = [];
+  for(let i = m; i <= n; i++){
+    for(let j = i; j <= n; j++){
+      result.push([i, j]);
+    }
+  }
+  return result;
+}
+
+console.log(generatePairs(2, 4));
