a526ff7de8b9347568e4369b4521484f32a3b3fe
LeeJams <leejam208@gmail.com>
2021-10-21 21:53:08 +0900

    업뎃

--- programmers/Gym_suit.js
+++ programmers/Gym_suit.js
@@ -17,19 +17,19 @@
   3	  [3]	      [1]	          2
 */
 function solution(n, lost, reserve) {
-  let fliterLost = lost.filter(x => !reserve.includes(x)).sort();
-  let filterReserve = reserve.filter(x => !lost.includes(x)).sort();
+  let fliterLost = lost.filter((x) => !reserve.includes(x)).sort();
+  let filterReserve = reserve.filter((x) => !lost.includes(x)).sort();
   let answer = n - fliterLost.length;
 
-  for(let i = 0; i < fliterLost.length; i++){
-    if(filterReserve.includes(fliterLost[i] - 1)) {
+  for (let i = 0; i < fliterLost.length; i++) {
+    if (filterReserve.includes(fliterLost[i] - 1)) {
       answer++;
-      const idx = filterReserve.findIndex(n => n === fliterLost[i] - 1);
+      const idx = filterReserve.findIndex((n) => n === fliterLost[i] - 1);
       filterReserve[idx] = 0;
       continue;
-    }else if(filterReserve.includes(fliterLost[i] + 1)) {
+    } else if (filterReserve.includes(fliterLost[i] + 1)) {
       answer++;
-      const idx = filterReserve.findIndex(n => n === fliterLost[i] + 1);
+      const idx = filterReserve.findIndex((n) => n === fliterLost[i] + 1);
       filterReserve[idx] = 0;
       continue;
     }
@@ -46,15 +46,32 @@ function solution(n, lost, reserve) {
 // console.log(solution(7, [1,4,5,6,7], [3,4,5,6,7])); // 7
 // console.log(solution(5, [4, 2], [3, 5])); // 5
 
-function solution_best(n, lost, reserve) {      
-    return n - lost.filter(a => {
-        console.log("a = ", a);
-        const b = reserve.find(r => Math.abs(r-a) <= 1)
-        console.log("b = ", b);
-        if(!b) return true
-        reserve = reserve.filter(r => r !== b)
-        console.log("reserve = ", reserve);
+function solution_best(n, lost, reserve) {
+  return (
+    n -
+    lost.filter((a) => {
+      console.log("a = ", a);
+      const b = reserve.find((r) => Math.abs(r - a) <= 1);
+      console.log("b = ", b);
+      if (!b) return true;
+      reserve = reserve.filter((r) => r !== b);
+      console.log("reserve = ", reserve);
     }).length
+  );
 }
-console.log(solution_best(7, [2,4,5,6,7], [1,3,4,5,6,7])); // 7
-console.log(solution_best(7, [2, 3, 4], [1, 2, 3, 6])) // 6
+console.log(solution_best(7, [2, 4, 5, 6, 7], [1, 3, 4, 5, 6, 7])); // 7
+console.log(solution_best(7, [2, 3, 4], [1, 2, 3, 6])); // 6
+
+function solution_update(n, lost, reserve) {
+  let fliterLost = lost.filter((x) => !reserve.includes(x)).sort();
+  let filterReserve = reserve.filter((x) => !lost.includes(x)).sort();
+
+  return (
+    n -
+    fliterLost.filter((a) => {
+      const b = reserve.find((r) => Math.abs(r - a) <= 1);
+      if (!b) return true;
+      filterReserve = filterReserve.filter((r) => r !== b);
+    }).length
+  );
+}
