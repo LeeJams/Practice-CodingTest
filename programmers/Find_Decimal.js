620cd6aec1164c05209f12e30526539502a3da9d
LeeJams <leejam208@gmail.com>
2021-10-24 01:16:21 +0900

    지리는 풀이...

--- programmers/Find_Decimal.js
+++ programmers/Find_Decimal.js
@@ -33,7 +33,7 @@
   return answer;
 } */
 
-function solution(n) {
+function solution_my(n) {
   const arr = [0, 0];
   for (let i = 2; i <= n; i++) {
     arr.push(i);
@@ -46,9 +46,27 @@ function solution(n) {
     }
   }
 
-  return arr.filter(n => n !== 0).length;
+  return arr.filter((n) => n !== 0).length;
+}
+
+//Set을 생성하면서 2의 배수는 삭제 제곱근을 구해 나머지도 삭제... 지린다...
+function solution(n) {
+  const s = new Set();
+  for (let i = 1; i <= n; i += 2) {
+    s.add(i);
+  }
+  s.delete(1);
+  s.add(2);
+  for (let j = 3; j < Math.sqrt(n); j++) {
+    if (s.has(j)) {
+      for (let k = j * 2; k <= n; k += j) {
+        s.delete(k);
+      }
+    }
+  }
+  return s.size;
 }
 
 console.time("solution");
-console.log(solution(100));
+console.log(solution(16));
 console.timeEnd("solution");
