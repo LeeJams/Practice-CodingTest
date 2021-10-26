cda690d01fd878f3d829127396d7f6574b82464c
LeeJams <leejam208@gmail.com>
2021-10-26 22:50:36 +0900

    진짜 신박하다!

--- codewars/Create_Phone_Number.js
+++ codewars/Create_Phone_Number.js
@@ -15,5 +15,19 @@ function createPhoneNumber(numbers){
   const third = numbers.slice(6);
   return `(${first.join("")}) ${second.join("")}-${third.join("")}`;
 }
-
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // (123) 456-7890
+
+function createPhoneNumber_best1(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+function createPhoneNumber_best2(numbers){
+  return numbers.reduce((p,c) => p.replace('x',c), "(xxx) xxx-xxxx");
+}
+console.log(createPhoneNumber_best([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // (123) 456-7890
