10d1607c78d3549fe22696895e558635cbe8e6ca
LeeJams <leejam208@gmail.com>
2021-11-02 14:30:43 +0900

    너무 깔끔한 한줄 코드..

--- codewars/Count_the_smiley_faces.js
+++ codewars/Count_the_smiley_faces.js
@@ -46,6 +46,10 @@ function countSmileys(arr) {
   return answer;
 }
 
+function countSmileys(arr) {
+  return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+
 console.log(countSmileys([])); // 0
 console.log(countSmileys([":D", ":~)", ";~D", ":)"])); // 4
 console.log(countSmileys([":)", ":(", ":D", ":O", ":;"])); // 2
