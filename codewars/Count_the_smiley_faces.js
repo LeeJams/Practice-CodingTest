b4c3bc10e3a5cd80c8dfad966fc8d6d2800aa23f
LeeJams <leejam208@gmail.com>
2021-11-03 09:39:36 +0900

    이런건 잘 되는데..

--- codewars/Count_the_smiley_faces.js
+++ codewars/Count_the_smiley_faces.js
@@ -54,3 +54,5 @@ console.log(countSmileys([])); // 0
 console.log(countSmileys([":D", ":~)", ";~D", ":)"])); // 4
 console.log(countSmileys([":)", ":(", ":D", ":O", ":;"])); // 2
 console.log(countSmileys([";]", ":[", ";*", ":$", ";-D"])); // 1
+
+console.log(/^[:;][-~]?[)D]$/.test(';D'))
