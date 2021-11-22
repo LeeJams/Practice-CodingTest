c2727f5a12494aa6a02a1d0adc210c95808cd015
LeeJams <leejam208@gmail.com>
2021-11-22 09:44:43 +0900

    쳌

--- working/Is_a_number_prime.js
+++ working/Is_a_number_prime.js
@@ -12,31 +12,31 @@ NOTE on performance: There are no fancy optimizations required, but still the mo
 Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
 */
 
-/* function isPrime(num) {
+function isPrime(num) {
   if(num < 2) return false;
-  for(let i = 3; i < Math.sqrt(num); i++){
+  for(let i = 2; i <= Math.sqrt(num); i++){
     if(num % i === 0){
       return false;
     }
   }
   return true;
-} */
-function isPrime(num) {
-  if(num < 2) return false;
-  const arr = [0, 0];
-  for (let i = 2; i <= num; i++) {
-    arr.push(i);
-  }
-
-  for (let i = 2; i <= num; i++) {
-    if (arr[i] == 0) continue;
-    for (let j = i + i; j <= num; j += i) {
-      arr[j] = 0;
-    }
-  }
-
-  return arr.includes(num);
 }
+// function isPrime(num) {
+//   if(num < 2) return false;
+//   const arr = [0, 0];
+//   for (let i = 2; i <= num; i++) {
+//     arr.push(i);
+//   }
+
+//   for (let i = 2; i <= num; i++) {
+//     if (arr[i] == 0) continue;
+//     for (let j = i + i; j <= num; j += i) {
+//       arr[j] = 0;
+//     }
+//   }
+
+//   return arr.includes(num);
+// }
 
 console.log(isPrime(0));
 console.log(isPrime(1));
