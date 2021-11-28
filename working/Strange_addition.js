d4c1b70ba368a092f6448364a226c731ecd7716d
LeeJams <leejam208@gmail.com>
2021-11-28 19:07:13 +0900

    다른 풀이

--- working/Strange_addition.js
+++ working/Strange_addition.js
@@ -19,17 +19,31 @@ You may assume both integers are positive integers.
 */
 
 function add(num1, num2) {
-  let num1Str = num1.toString();
-  let num2Str = num2.toString();
+  const num1Str = num1.toString();
+  const num2Str = num2.toString();
   const max = Math.max(num1Str.length, num2Str.length);
 
-  const no1 = num1Str.padStart(max, '0').split('');
-  const no2 = num2Str.padStart(max, '0').split('');
-  
-  return parseInt(no1.map((n, idx) => parseInt(n) + parseInt(no2[idx])).join(''));
+  const no1 = num1Str.padStart(max, "0").split("");
+  const no2 = num2Str.padStart(max, "0").split("");
+
+  return parseInt(
+    no1.map((n, idx) => parseInt(n) + parseInt(no2[idx])).join("")
+  );
 }
 
-console.log(add(2, 11)); // 13
-console.log(add(0, 1)); // 1
-console.log(add(16, 18)); // 214
-console.log(add(122, 81)); // 1103
+function add_another(num1, num2) {
+  let arr1 = num1.toString().split("").reverse();
+  let arr2 = num2.toString().split("").reverse();
+
+  let res = [];
+
+  for (let i = 0; i < Math.max(arr1.length, arr2.length); i++) {
+    res.push((+arr2[i] || 0) + (+arr1[i] || 0));
+  }
+  return +res.reverse().join("");
+}
+
+console.log(add_another(2, 11)); // 13
+console.log(add_another(0, 1)); // 1
+console.log(add_another(16, 18)); // 214
+console.log(add_another(122, 81)); // 1103
