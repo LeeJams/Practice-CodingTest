03e4af47985c931aaf3dcd70880a2f7f66c29238
LeeJams <leejam208@gmail.com>
2021-12-09 22:14:10 +0900

    똑똑이들..

--- working/Word_chain_game.js
+++ working/Word_chain_game.js
@@ -68,6 +68,16 @@ function solution(n, words) {
   return [0, 0];
 }
 
+function solution(n, words) {
+    let answer = 0;
+    words.reduce((prev, now, idx) => {
+        answer = answer || ((words.slice(0, idx).indexOf(now) !== -1 || prev !== now[0]) ? idx : answer);
+        return now[now.length-1];
+    }, "")
+
+    return answer ? [answer%n+1, Math.floor(answer/n)+1] : [0,0];
+}
+
 console.log(solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])); // [3, 3]
 console.log(solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"])); // [0, 0]
 console.log(solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])); // [1, 3]
