a7decc5a50f0147f5daf0a995e8a6120a9b06ef8
LeeJams <leejam208@gmail.com>
2022-01-11 13:26:38 +0900

    새롭게

--- practice/Recursive.js
+++ practice/Recursive.js
@@ -1,74 +1,131 @@
-const data = [
-  {
-    id: 4,
-    name: "김수사",
-    parents: 1,
-  },
-  {
-    id: 5,
-    name: "김수오",
-    parents: 2,
-  },
-  {
-    id: 1,
-    name: "김수일",
-    parents: null,
-  },
-  {
-    id: 2,
-    name: "김수이",
-    parents: null,
-  },
-  {
-    id: 3,
-    name: "김수삼",
-    parents: 1,
-  },
-  {
-    id: 6,
-    name: "김수육",
-    parents: 5,
-  },
-  {
-    id: 7,
-    name: "김수칠",
-    parents: 2,
-  },
-];
-
+const result = require("./data.json");
 // 순서가 보장 되지 않을 경우 문제가 발생함..
-console.time("step1");
-const dataList = [];
-
-const recursive = (nodes, item) => {
-  nodes.forEach(node => {
-    if(node.id === item.parents) return node.children.push({...item, children: []});
-    else recursive(node.children, item);
-  })
+function firstWay() {
+  const dataList = [];
+  
+  const recursive = (nodes, item) => {
+    nodes.forEach((node) => {
+      if (node.distSeqNo === item.parentDistSeqNo) {
+        node.children.push({ ...item, children: [] });
+      } else {
+        recursive(node.children, item);
+      }
+    });
+  };
+  
+  result.data.forEach((item) => {
+    if (item.parentDistSeqNo == null) {
+      dataList.push({
+        ...item,
+        children: [],
+      });
+    } else {
+      recursive(dataList, item);
+    }
+  });
 }
-
-data.forEach(item => {
-  if(item.parents === null){
-    dataList.push({...item, children: []});
-  }else{
-    recursive(dataList, item);
-  }
-})
-console.timeEnd("step1");
 
 /* -------------------- */
-console.time("step2");
-const dataList2 = data.filter(n => n.parents === null).map(n => ({...n, children: []}));
-const items = data.filter(n => n.parents !== null);
-
-const recursive2 = (nodes, item) => {
-  nodes.forEach(node => {
-    if(node.id === item.parents) node.children.push({...item, children: []});
-    else recursive2(node.children, item);
-  })
+function secondWay() {
+  const dataList2 = result.data
+    .filter((n) => n.parentDistSeqNo === null)
+    .map((n) => ({ ...n, children: [] }));
+  const items = result.data.filter((n) => n.parentDistSeqNo !== null);
+  
+  const recursive2 = (nodes, item) => {
+    nodes.forEach((node) => {
+      if (node.distSeqNo === item.parentDistSeqNo)
+        node.children.push({ ...item, children: [] });
+      else recursive2(node.children, item);
+    });
+  };
+  
+  items.forEach((item) => {
+    recursive2(dataList2, item);
+  });
 }
 
-items.forEach(item => {
-  recursive2(dataList2, item);
-})
-console.timeEnd("step2");
+/* -------------------- */
+const result2 = {
+  data: [
+    {
+      "distSeqNo": 1,
+      "distLevel": 1,
+      "parentDistSeqNo": null,
+    },
+    {
+      "distSeqNo": 2,
+      "distLevel": 1,
+      "parentDistSeqNo": null,
+    },
+    {
+      "distSeqNo": 3,
+      "distLevel": 1,
+      "parentDistSeqNo": null,
+    },
+    {
+      "distSeqNo": 4,
+      "distLevel": 2,
+      "parentDistSeqNo": 3,
+    },
+    {
+      "distSeqNo": 5,
+      "distLevel": 2,
+      "parentDistSeqNo": 2,
+    },
+    {
+      "distSeqNo": 6,
+      "distLevel": 3,
+      "parentDistSeqNo": 5,
+    },
+    {
+      "distSeqNo": 7,
+      "distLevel": 4,
+      "parentDistSeqNo": 6,
+    },
+    {
+      "distSeqNo": 8,
+      "distLevel": 5,
+      "parentDistSeqNo": 7,
+    },
+    {
+      "distSeqNo": 9,
+      "distLevel": 6,
+      "parentDistSeqNo": 8,
+    },
+    {
+      "distSeqNo": 10,
+      "distLevel": 7,
+      "parentDistSeqNo": 9,
+    },
+  ]
+}
+function thirdWay() {
+  for (let i = result.data[result.data.length - 1].distLevel; i > 1; i--) {
+    const data = result.data.filter((n) => n.distLevel === i);
+    data.forEach((item) => {
+      const idx = result.data.findIndex(
+        (n) => n.distSeqNo === item.parentDistSeqNo
+      );
+      if(!result.data[idx].children) result.data[idx].children = [];
+      result.data[idx].children.push(item);
+    });
+  }
+}
+/* function thirdWay() {
+  for(let i = 7; i > 1; i--){
+    const data = result2.data.filter(n => n.distLevel === i);
+    data.forEach(item => {
+      const idx = result2.data.findIndex(n => n.distSeqNo === item.parentDistSeqNo);
+      result2.data[idx].children = [];
+      result2.data[idx].children.push(item);
+    })
+  }
+} */
+
+console.time("test");
+firstWay();
+console.timeEnd("test");
+console.time("test");
+thirdWay();
+console.timeEnd("test");
