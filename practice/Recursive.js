2075850f6597ac01d671a9c8ff31bd4c6356ca91
LeeJams <leejam208@gmail.com>
2022-01-11 10:09:46 +0900

    filter가 성능이 더 좋다..?

--- practice/Recursive.js
+++ practice/Recursive.js
@@ -1,5 +1,15 @@
 const data = [
   {
+    id: 4,
+    name: "김수사",
+    parents: 1,
+  },
+  {
+    id: 5,
+    name: "김수오",
+    parents: 2,
+  },
+  {
     id: 1,
     name: "김수일",
     parents: null,
@@ -15,16 +25,6 @@ const data = [
     parents: 1,
   },
   {
-    id: 4,
-    name: "김수사",
-    parents: 1,
-  },
-  {
-    id: 5,
-    name: "김수오",
-    parents: 2,
-  },
-  {
     id: 6,
     name: "김수육",
     parents: 5,
@@ -35,6 +35,9 @@ const data = [
     parents: 2,
   },
 ];
+
+// 순서가 보장 되지 않을 경우 문제가 발생함..
+console.time("step1");
 const dataList = [];
 
 const recursive = (nodes, item) => {
@@ -51,5 +54,21 @@ data.forEach(item => {
     recursive(dataList, item);
   }
 })
+console.timeEnd("step1");
 
-console.log(dataList);
+/* -------------------- */
+console.time("step2");
+const dataList2 = data.filter(n => n.parents === null).map(n => ({...n, children: []}));
+const items = data.filter(n => n.parents !== null);
+
+const recursive2 = (nodes, item) => {
+  nodes.forEach(node => {
+    if(node.id === item.parents) node.children.push({...item, children: []});
+    else recursive2(node.children, item);
+  })
+}
+
+items.forEach(item => {
+  recursive2(dataList2, item);
+})
+console.timeEnd("step2");
