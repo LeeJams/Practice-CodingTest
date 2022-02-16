4c08ff65323eceb669ca10347d86d85216adb287
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-02-16 22:18:08 +0900

    실력부족..

--- working/Triangle_Snail.js
+++ working/Triangle_Snail.js
@@ -1,4 +1,5 @@
 /* 
+삼각 달팽이
 https://programmers.co.kr/learn/courses/30/lessons/68645
 
 정수 n이 매개변수로 주어집니다. 다음 그림과 같이 밑변의 길이와 높이가 n인 삼각형에서 맨 위 꼭짓점부터 반시계 방향으로 달팽이 채우기를 진행한 후, 
@@ -33,12 +34,17 @@ n   result
 [1] [2,15] [3,16,14] [4,17,21,13] [5,18,19,20,12] [6,7,8,9,10,11]
 */
 function solution(n) {
-  let count = 1;
-  for(let i = 0; i < n; i++){
-    for(let j = 0; j <= i; j++){
-      console.log(count++);
-    }
+  const arr = [];
+  
+  let count = (n*(n+1))/2
+
+  for(let i = 1; i <= n; i++){
+    arr.push([i]);
   }
+  for(let i = n + 1; i < n + n; i++){
+    arr[n - 1].push(i);
+  }
+  console.log(arr);
 }
 
 console.log(solution(4)); // [1, 2, 9, 3, 10, 8, 4, 5, 6, 7]
