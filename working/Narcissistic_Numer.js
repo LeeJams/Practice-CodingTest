eaf56677e6d2afc5033e6cc6dba726478d85081d
LeeJams <89899636+LeeJams@users.noreply.github.com>
2022-05-28 07:41:51 +0900

    나르시스틱

---
+++ working/Narcissistic_Numer.js
@@ -0,0 +1,18 @@
+/* 
+https://www.codewars.com/kata/56b22765e1007b79f2000079/train/javascript
+
+A Narcissistic Number is a number of length l in which the sum of its digits to the power of l is equal to the original number. If this seems confusing, refer to the example below.
+
+Ex: 153, where l = 3 ( the number of digits in 153 )
+13 + 53 + 33 = 153
+
+Write a function that, given n, returns whether or not n is a Narcissistic Number.
+*/
+
+function isNarcissistic(n) {
+  return n.toString().split('').map((n, idx, arr) => parseInt(n) ** arr.length).reduce((acc, cur) => acc + cur) === n
+}
+
+console.log(isNarcissistic(153)) // true
+console.log(isNarcissistic(1)) // true
+console.log(isNarcissistic(435)) // false
