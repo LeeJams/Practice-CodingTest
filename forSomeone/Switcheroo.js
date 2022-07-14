2540f856c8b9d9068c1aea646bb0a7ca53097c0c
LeeJams <leejam208@gmail.com>
2022-07-14 14:18:23 +0900

    switch

---
+++ forSomeone/Switcheroo.js
@@ -0,0 +1,23 @@
+/* 
+  https://www.codewars.com/kata/57f759bb664021a30300007d/train/javascript
+
+  a, b, c로 구성되어 있는 문자열이 매개변수로 주어집니다.
+
+  해당 문자열으 a는 b로 b는 a로 변환하는 작업을 해주세요.
+
+  단 c는 그대로 c입니다.
+
+  Example:
+
+  'acb' --> 'bca'
+  'aabacbaa' --> 'bbabcabb'
+*/
+
+function switcheroo(x){
+  // TODO
+}
+
+
+console.log(switcheroo_best('abbacbabca')); // baabcabacb
+console.log(switcheroo_best('aaabcccbaaa')); // bbbacccabbb
+console.log(switcheroo_best('ccccc')); // ccccc
