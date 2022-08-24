f043333632ac9b122e139444b1612926cd0abdab
LeeJams <woals214@gmail.com>
2022-08-24 16:48:54 +0900

    순열과 조합

--- 조합.js
+++ 조합.js
@@ -1,20 +1,23 @@
-const getCombinations = function (arr, selectNumber) {
-  const results = [];
-  if (selectNumber === 1) return arr.map((el) => [el]);
-  // n개중에서 1개 선택할 때(nC1), 바로 모든 배열의 원소 return
+function combinations(arr, n) {
+  // 1개만 뽑는다면 그대로 조합을 반환한다. 탈출 조건으로도 사용된다.
+  if (n === 1) return arr.map((v) => [v]);
+  const result = [];
 
-  arr.forEach((fixed, index, origin) => {
-    const rest = origin.slice(index + 1);
-    // 해당하는 fixed를 제외한 나머지 뒤
-    const combinations = getCombinations(rest, selectNumber - 1);
-    // 나머지에 대해서 조합을 구한다.
-    const attached = combinations.map((el) => [fixed, ...el]);
-    //  돌아온 조합에 떼 놓은(fixed) 값 붙이기
-    results.push(...attached);
-    // 배열 spread syntax 로 모두다 push
+  // 요소를 순환한다
+  arr.forEach((fixed, idx, arr) => {
+    // 현재 index 이후 요소를 추출한다.
+    // index번째는 선택된 요소
+    const rest = arr.slice(idx + 1);
+    // 선택된 요소 이전 요소들을 제외하고 재귀 호출한다.
+    const combis = combinations(rest, n - 1);
+    // 선택된 요소와 재귀 호출을 통해 구한 조합을 합쳐준다.
+    const combine = combis.map((v) => [fixed, ...v]);
+    // 결과 값을 추가한다.
+    result.push(...combine);
   });
 
-  return results; // 결과 담긴 results return
-};
+  // 결과 반화
+  return result;
+}
 
-console.log(getCombinations([1, 2, 3, 4], 3));
+console.log(combinations([1, 2, 3, 4], 3));
