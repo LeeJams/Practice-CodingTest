03dd09e828eeb795d03873198d0cdcf0548dcde6
LeeJams <woals214@gmail.com>
2022-09-02 18:02:35 +0900

    백트레킹 모르겠다..

--- programmers/강의/N-Queen.js
+++ programmers/강의/N-Queen.js
@@ -14,9 +14,40 @@ n은 12이하의 자연수 입니다.
 n	  result
 4	  2
 */
+function check(queen, row) {
+  console.log('check - queen = ', queen)
+  console.log('check - row = ', row)
+  // 이전까지 두었던 퀸의 위치를 확인한다.
+  for (let i = 0; i < row; i += 1) {
+      // 행의 위치와 대각선의 위치를 체크한다.
+      if (queen[i] === queen[row] || Math.abs(queen[i] - queen[row]) === row - i) {
+          return false; // 둘 수 없다면 false
+      }
+  }
+
+  return true; // 모두 통과되면 true
+}
+function search(queen, row) {
+  const n = queen.length;
+  let count = 0;
+
+  if (n === row) { // 체스판 끝에 도달했다면.. 재귀의 탈출 조건
+      return 1;
+  }
+
+  for (let col = 0; col < n; col += 1) { // 0부터 n까지 열을 돌면 둘 수 있게 만든다.
+      queen[row] = col; // 우선 퀸을 둔다
+      if (check(queen, row)) { // 퀸을 둘 수 있다면..
+        const nu = search(queen, row + 1); // 다음 행으로 이동!
+        console.log('count + ', nu)
+        count += nu
+      }
+  }
+
+  return count;
+}
 function solution(n) {
-  const chess = Array(n).fill(Array(n))
-  console.log(chess)
+  return search(Array.from({ length: n }, () => 0), 0);
 }
 
 console.log(solution(4));
