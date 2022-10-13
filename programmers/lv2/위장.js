e0b5f973052c58138ca806e940a8a24037ad8b6d
LeeJams <woals214@gmail.com>
2022-10-13 10:33:42 +0900

    위장

--- programmers/lv2/위장.js
+++ programmers/lv2/위장.js
@@ -36,15 +36,35 @@ headgear에 해당하는 의상이 yellow_hat, green_turban이고 eyewear에 해
 5. green_turban + blue_sunglasses
 */
 
+/* 
+만약에 옷의 종류가 1개라고 해봅시다. 개수는 a개입니다.
+그럼 총 a가지의 경우가 있겠죠?
+
+종류가 2개가 되고 각각의 옷의 개수는 a, b개입니다.
+그럼 경우의 수는 a, b, ab가 되므로 조합의 개수는 (a+b) + (ab)가지입니다.
+
+3개가 된다면? (a+b+c) + (ab+bc+ca) + (abc)가지입니다.
+
+어디서 많이 보시지 않았나요? 학창시절에 우리는 다항식을 배우는데 위의 가짓수는 n차식(n = 옷의 종류의 개수) 계수들의 합입니다.
+
+즉 옷의 종류가 3가지고 각각의 옷의 개수가 a, b, c라면 (x+a)(x+b)(x+c) = x3 + (a+b+c)x2 + (ab+bc+ca)x + (abc)라는 식이 정립됩니다. 보이시죠? 총 조합의 개수가 계수에 다 포함되어 있습니다.
+
+해당 식의 계수의 합을 구하려면 x=1을 대입해주면 됩니다. 그 후 맨 앞 x3 의 계수는 정답에 포함되지 않으므로 마지막에 1을 빼주는 겁니다.
+x=1을 대입한 식은 (1+a)(1+b)(1+c)가 되고 그 값에 1을 뺀 후 리턴해주면 정답이 나오는 이유가 그것입니다.
+*/
+
 function solution(clothes) {
   const obj = {};
-  let result = 0;
-  clothes.forEach((item) => {
-    obj[item[1]] = obj[item[1]] ? obj[item[1]] + 1 : 1;
-    result++;
+  clothes.forEach((n) => {
+    obj[n[1]] = ++obj[n[1]] || 1;
   });
 
-  return result;
+  let answer = 1;
+  for (let key in obj) {
+    answer *= obj[key] + 1;
+  }
+
+  return answer - 1;
 }
 
 console.log(
