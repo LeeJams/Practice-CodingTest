ef1096be254b8bad3bb5f33b294ed9be685e8a88
LeeJams <woals214@gmail.com>
2023-02-13 00:35:49 +0900

    무인도여행

--- programmers/lv2/무인도여행.js
+++ programmers/lv2/무인도여행.js
@@ -5,6 +5,7 @@ function solution(maps) {
   const visite = Array.from({length: maps.length}, () => Array(maps[0].length).fill(true));
   const dx = [1, 0, -1, 0];
   const dy = [0, 1, 0, -1];
+  
   let sum = 0;
   function DFS(x, y, num){
       sum += Number(num);
@@ -13,7 +14,6 @@ function solution(maps) {
           const ny = y + dy[i];
           if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length) {
               if(visite[nx][ny] && maps[nx][ny] !== 'X'){
-                  console.log(nx, ny);
                   visite[nx][ny] = false;
                   DFS(nx, ny, maps[nx][ny]);
               }
@@ -26,15 +26,13 @@ function solution(maps) {
       for(let j = 0; j < maps[0].length; j++){
           if(visite[i][j] && maps[i][j] !== 'X') {
               sum = 0;
-              console.log(i, j);
               visite[i][j] = false;
               DFS(i, j, maps[i][j]);
               answer.push(sum);
           }
       }
   }
-  
-  return answer;
+  return answer.length ? answer.sort((a, b) => a - b) : [-1];
 }
 
-console.log(solution(["X591X","X1X5X","X231X", "1XXX1"])); // [1, 1, 27]
+console.log(solution(["X591X", "X1X5X", "X231X", "1XXX1"])); // [1, 1, 27]
